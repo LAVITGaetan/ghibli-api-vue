fff6d24f028179619a78efeab0b00f47ac404e41
Gaetan LAVIT <g.lavit@outlook.fr>
2022-06-03 21:50:46 +0400

    Affichage d'autres données et amélioration css

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="app">
-    <div class="modal" v-if="film.length > 0"><h3 class="modal-title" v-for="film in film" :key="film.id"> {{film.title}}</h3><div @click="resetMovie" class="modal-close">Fermer</div></div>
+    <div class="modal" v-if="film.length > 0">
+      <h3 class="modal-title" v-for="film in film" :key="film.id">
+        {{ film.title }}
+      </h3>
+      <ul class="modal-list">
+        <li v-for="film in film" :key="film.id">{{ film.original_title_romanised }}</li>
+        <li v-for="film in film" :key="film.id">{{ film.original_title }}</li>
+        <li v-for="film in film" :key="film.id">{{ film.producer }}</li>
+        <li v-for="film in film" :key="film.id">{{ film.release_date }}</li>
+      </ul>
+      <div @click="resetMovie" class="modal-close">Fermer</div>
+    </div>
     <h1 class="title">Ghibli Movies</h1>
     <div class="flex" v-if="films.length > 0">
       <div class="card" v-for="film in films" :key="film.id">
-        <img class="card-thumbnail" v-bind:src="film.image" v-bind:alt="film.title" />
+        <img
+          class="card-thumbnail"
+          v-bind:src="film.image"
+          v-bind:alt="film.title"
+        />
         <div class="card-action" @click="showMovie(film.id)">+</div>
       </div>
     </div>
@@ -18,7 +33,7 @@ export default {
     return {
       api_uri: "https://ghibliapi.herokuapp.com/films",
       films: {},
-      film: []
+      film: [],
     };
   },
   methods: {
@@ -33,13 +48,13 @@ export default {
     },
     showMovie(movieId) {
       this.film = [];
-      let film = this.films.find(movie => movie.id == movieId);
+      let film = this.films.find((movie) => movie.id == movieId);
       this.film.push(film);
       console.log(this.film[0]);
     },
     resetMovie() {
-      this.film = []
-    }
+      this.film = [];
+    },
   },
   beforeMount() {
     this.fetchApi();
@@ -55,14 +70,15 @@ export default {
 }
 
 body {
-  background: #f5fafb;
+  background: #e7fcff;
+  font-family: monospace;
 }
 
 .modal {
   position: fixed;
   width: 300px;
   height: fit-content;
-  padding: 25px 25px 10px 25px;
+  padding:25px;
   background: #ffffff;
   box-shadow: 5px 5px 100px 10px #00000050;
   top: 25vh;
@@ -71,16 +87,28 @@ body {
   border-radius: 5px;
 }
 
+.modal-list {
+  width: 90%;
+  margin: 30px auto 0 auto;
+  display: block;
+  list-style-type: none;
+}
+
 .modal-close {
   background: #181826;
   color: #ffffff;
   font-size: 16px;
-  margin: 50px auto 0 auto;
-  padding: .7em 1.4em;
+  margin: 30px auto 0 auto;
+  padding: 0.7em 1.4em;
   border-radius: 50px;
   text-align: center;
   widows: fit-content;
-  cursor : pointer;
+  cursor: pointer;
+}
+
+.modal-close:hover {
+  transition: .3s ease-in;
+  background: #13e4e4;
 }
 
 .modal-title {
@@ -88,6 +116,10 @@ body {
   color: #181826;
   text-align: center;
   font-weight: bold;
+  border-bottom: 4px solid #13e4e4;
+  width: 90%;
+  margin: auto;
+  padding-bottom: 10px;
 }
 
 .title {
@@ -114,13 +146,14 @@ body {
 }
 
 .card-action {
+  transition: 0.3s ease;
   position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
   box-shadow: 5px 5px 10px 1px #00000020;
   z-index: 100;
-  width: 150px;
+  width: 64px;
   height: 64px;
   background: #13e4e4ea;
   color: #fff;
@@ -128,13 +161,15 @@ body {
   font-weight: bold;
   border-radius: 50px;
   cursor: pointer;
-  left: calc(50% - 75px);
+  left: calc(50% - 32px);
   bottom: -32px;
 }
 
 .card-action:hover {
-  transition: .3s ease;
-  background: #13E4E4;
+  transition: 0.3s ease;
+  left: calc(50% - 75px);
+  width: 150px;
+  background: #13e4e4;
 }
 
 .card-thumbnail {
